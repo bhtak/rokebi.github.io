5ef3bff10e12b08825270954491e652185acca47
Jisong <jisonglee@uangel.com>
2020-04-03 09:15:48 +0900

    add example post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Example",
+        "excerpt":"포스트 예시     ","categories": ["Example"],
+        "tags": ["Example"],
+        "url": "http://localhost:4000/example/example/",
+        "teaser": null
+      }]
